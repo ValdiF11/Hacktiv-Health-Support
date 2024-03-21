42f15c731e75a06172a46d2d468b8d5d0525a5b9
ValdiF11 <valdifirstianto@gmail.com>
2024-03-21 22:44:30 +0700

    valdi

--- Controllers/patientController.js
+++ Controllers/patientController.js
@@ -1,55 +1,59 @@
 const { Doctor, Patient, DoctorPatient, HealthParameter } = require("../models");
 
 class patientController {
-    static async showAppointment(req, res) {
-        try {
-            const doctors = await Doctor.findAll();
-            res.render('showformappointment', { doctors: doctors });
-        } catch (error) {
-            res.send(error);
-        }
+  static async showAppointment(req, res) {
+    try {
+      const doctors = await Doctor.findAll();
+      res.render("showformappointment", { doctors: doctors });
+    } catch (error) {
+      res.send(error);
     }
-    static async showMedical(req, res) {
-        try {
-            const patientId = req.params.patientId;
-            const patients = await Patient.findAll({
-                where: {
-                    id:patientId
-                },
-                include: [{
-                    model: DoctorPatient,
-                    include: [{
-                        model: Doctor,
-                        attributes: ['name']
-                    }]
-                }]
-            });
-            res.render('showTableMedical', { patients: patients });
-        } catch (error) {
-            res.send(error);
-        }
+  }
+  static async showMedical(req, res) {
+    try {
+      const patientId = req.params.patientId;
+      const patients = await Patient.findAll({
+        where: {
+          id: patientId,
+        },
+        include: [
+          {
+            model: DoctorPatient,
+            include: [
+              {
+                model: Doctor,
+                attributes: ["name"],
+              },
+            ],
+          },
+        ],
+      });
+      res.render("showTableMedical", { patients: patients });
+    } catch (error) {
+      res.send(error);
     }
-    static async showHealth(req, res) {
-        try {
-            let data = HealthParameter.findAll();
-            res.render('showTableHealth', { data });
-        } catch (error) {
-            res.send(error);
-        }
+  }
+  static async showHealth(req, res) {
+    try {
+      let data = HealthParameter.findAll();
+      res.render("showTableHealth", { data });
+    } catch (error) {
+      res.send(error);
     }
-    static async deleteHealth(req, res) {
-        try {
-            const { PatientsId, HealthId } = req.params;
-            await HealthParameter.destroy({
-                where: {
-                    id: HealthId
-                }
-            });
-            res.redirect(`showTableHealth/${PatientsId}`)
-        } catch (error) {
-            res.send(error);
-        }
+  }
+  static async deleteHealth(req, res) {
+    try {
+      const { PatientsId, HealthId } = req.params;
+      await HealthParameter.destroy({
+        where: {
+          id: HealthId,
+        },
+      });
+      res.redirect(`showTableHealth/${PatientsId}`);
+    } catch (error) {
+      res.send(error);
     }
+  }
 }
 
 module.exports = patientController;
