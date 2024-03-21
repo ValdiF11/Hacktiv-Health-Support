138432d42dd01c080eabc4ef562d878aaa716560
ValdiF11 <valdifirstianto@gmail.com>
2024-03-21 22:18:53 +0700

    valdi

--- Controllers/patientController.js
+++ Controllers/patientController.js
@@ -1,34 +1,38 @@
-const { Doctor, Patient,DoctorPatient } = require("../models");
+const { Doctor, Patient, DoctorPatient } = require("../models");
 
 class patientController {
-    static async showAppointment(req, res) {
-        try {
-            const doctors = await Doctor.findAll();
-            res.render('showformappointment', { doctors: doctors });
-        } catch (error) {
-            res.send(error);
-        }
+  static async showAppointment(req, res) {
+    try {
+      const doctors = await Doctor.findAll();
+      res.render("showformappointment", { doctors: doctors });
+    } catch (error) {
+      res.send(error);
     }
-    static async showMedical(req, res) {
-        try {
-            const patientId = req.params.patientId;
-            const patients = await Patient.findAll({
-                where: {
-                    id:patientId
-                },
-                include: [{
-                    model: DoctorPatient,
-                    include: [{
-                        model: Doctor,
-                        attributes: ['name'] 
-                    }]
-                }]
-            });
-            res.render('showTableMedical', { patients: patients });
-        } catch (error) {
-            res.send(error);
-        }
+  }
+  static async showMedical(req, res) {
+    try {
+      const patientId = req.params.patientId;
+      const patients = await Patient.findAll({
+        where: {
+          id: patientId,
+        },
+        include: [
+          {
+            model: DoctorPatient,
+            include: [
+              {
+                model: Doctor,
+                attributes: ["name"],
+              },
+            ],
+          },
+        ],
+      });
+      res.render("showTableMedical", { patients: patients });
+    } catch (error) {
+      res.send(error);
     }
+  }
 }
 
 module.exports = patientController;
